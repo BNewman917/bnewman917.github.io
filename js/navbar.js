647770ad1229cb3f0268cbd89c1242c732468044
BNewman917 <brentnewman917@gmail.com>
2022-03-26 09:13:58 -0500

    projects link

--- js/navbar.js
+++ js/navbar.js
@@ -13,7 +13,7 @@ const myNavBar = `
       >
     </li>
     <li class="nav-item">
-      <a class="nav-link active" href="./html/projects.html">Projects</a>
+      <a class="nav-link active" href="./projects.html">Projects</a>
     </li>
     <li class="nav-item">
       <a
